6ade64f7756d18497b201fd221ab85d5f27de40a
thomasjrye <thomasjrye@gmail.com>
2020-09-12 18:50:35 +0200

    fiksa 4

--- oppgaver/oppgave4.java
+++ oppgaver/oppgave4.java
@@ -10,27 +10,23 @@ import java.util.Arrays;
 public class oppgave4 {
 
 	public static void main(String[] args) {
-
-		int a = Integer.parseInt(showInputDialog("tall1 :"));
-
-		int b = Integer.parseInt(showInputDialog("tall1 :"));
-
-		int c = Integer.parseInt(showInputDialog("tall1 :"));
-
-		if (a>=b && b>=c) {
-			System.out.println(a + " " + b + " " + c);
-		} if (a>c && c>b) {
-			System.out.println(a + " " + c + " " + b);
-		} if (b>a && a>c) {
-			System.out.println(b + " " + a + " " + c);
-		}  if (b>c && c>a) {
-			System.out.println(b + " " + c + " " + a);
-		} if (c>b && b>a) {
-			System.out.println(c + " " + b + " " + a);
-		} if (c>a && a>b) {
-			System.out.println(c + " " + a + " " + b);
+		Scanner myObj = new Scanner(System.in);
+		
+		double skatt = 0;
+		
+		System.out.println("Inntekt?:");
+		int inntekt = Integer.parseInt(myObj.nextLine());
+		if (inntekt > 934050) {
+			skatt = inntekt*0.1452;
+		} else if (inntekt <= 934050 && inntekt > 580650) {
+			skatt = inntekt*0.1152;
+		} else if (inntekt <= 580650 && inntekt > 230950) {
+			skatt = inntekt*0.0241;
+		} else if (inntekt <= 230950 && inntekt > 164100) {
+			skatt = inntekt*0.0093;
 		} 
-		
-		
+		System.out.println("inntekt: " + (inntekt-skatt));
+		System.out.print("trinnskatt: " + skatt);
+
 	}
 }
